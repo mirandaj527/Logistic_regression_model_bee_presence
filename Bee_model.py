d247b7c45a9ec43adb8b70a9b0df62b2d134b378
Miranda Johnstone <mk.chicot@gmail.com>
2025-12-16 21:48:21 +0000

    Update Bee_model.py
    Further code tidying, added in subheadings

--- Bee_model.py
+++ Bee_model.py
@@ -1,6 +1,6 @@
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 # 1. Loading necessary libraries and resources
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 
 import pandas as pd
 import numpy as np
@@ -9,18 +9,23 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.feature_selection import RFE
+from sklearn.metrics import adjusted_rand_score
 
-# -----------------------------
+from pandas.plotting import scatter_matrix
+
+# ------------------------------------------------------------------------------------- #
 # 2. Reading in and cleaning data
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 
+### READING IN BEE COMMUNITY DATA ###
 # Reading in bee community data - describes bee presence for each site
-df = pd.read_csv("./Community_Data.csv")
-
+df = pd.read_csv("Community_Data.csv")
 # Checking data looks ok
 df.head()
+
+### ADDING TOTAL NUMBER OF SPECIES AND TOTAL NUMBER OF BEES ###
 
 # Counting total number of species present and adding it to the data frame (column = total_species)
 # We do this by isolating all the columns containing species counts (so all but the first), then we count the number of these columns which contain a number greater than 0
@@ -31,10 +36,12 @@ df["total_species"] = (df[species_cols] > 0).sum(axis=1)
 # We do this by adding up all the bee counts across the species columns
 df["total_bees"] = df[species_cols].sum(axis=1)
 
-# Calculating simpson's diversity index for each site and adding it to the data frame (column = diverity)
+### CALCULATING SIMPSONS DIVERSITY INDEX AND ADDING IT TO DATA TABLE ###
 
+# Calculating simpson's diversity index for each site and adding it to the data frame (column = diverity)
 # Calculate proportions (n/N for each species, n = total number of focal species, N = total number of bees overall)
-proportions = df[species_cols].div(df["total_bees"], axis=0)
+proportions = df[species_cols].div(df["total_bees"].replace(0, np.nan), axis=0)
+# We make sure that any rows where total number of bees or species = 0 get a diversity index of 0 (otherwise the formula would return NaNs)
 
 # Simpson's Diversity Index (1 - sum((n/N)^2))
 df["diversity"] = 1 - (proportions ** 2).sum(axis=1)
@@ -44,6 +51,8 @@ df["diversity"] = 1 - (proportions ** 2).sum(axis=1)
 print(df["diversity"].describe())
 print("\nNumber of NAs:", df["diversity"].isna().sum())
 
+### COMPARING DIFFERENT DIVERSITY METRICS (SIMPSONS DIVERSITY INDEX VS SPECIES NUMBER) ###
+
 # Histogram of diversity data
 df["diversity"].hist()
 # Very non-normal data, heavily left-skewed
@@ -52,46 +61,44 @@ df["diversity"].hist()
 df["total_species"].hist()
 # More normal looking, but a bit right skewed
 
-# Method 1: Classifying sites as 'high' or 'low' diversity
+# Method 1: Classifying sites as 'high' or 'low' diversity based on simpsons diversity index
 # High diversity = simpsons diversity index higher than the median
 # Low diversity = simpsons diversitt index lower than the median
 # Creating a column to store this classification (divseristy_class_simpsons)
-
 simpson_median = df["diversity"].median()
-
 df["diversity_class_simpsons"] = np.where(
     df["diversity"] > simpson_median,
     "high",
     "low"
 )
-
 # Checking to see how many there are of each category (high diversity vs low diversity sites)
 df["diversity_class_simpsons"].value_counts()
 
-# Method 2: Classifying sites as 'high' or 'low' diversity
+# Method 2: Classifying sites as 'high' or 'low' diversity based on total species number
 # High diversity = number of species higher than the median
 # Low diversity = number of species lower than the median
 # Creating a column to store this classification (divseristy_class_species)
-
 species_median = df["total_species"].median()
 print(species_median)
-
 df["diversity_class_species"] = np.where(
     df["total_species"] > species_median,
     "high",
     "low"
 )
-
 # Checking to see how many there are of each category (high diversity vs low diversity sites)
 df["diversity_class_species"].value_counts()
 
+### READING IN SITE FEATURE DATA ###
+
 # Reading in site features data (things like latitude, longitude, water cover, forest cover, etc.)
 site_data = pd.read_csv("Site_Data.csv")
-
 # Check it looks ok
 site_data.head()
 
-# Creating a data table containing the bee community columns I want to keep
+### CREATING A MERGED DATA FRAME (uses a subset of columns from both the bee community data frame and the site feature data frame) ###
+
+# Creating a data table containing only the bee community columns I want to keep
+# We no longer needs counts for each specific species, just these diversity metrics will do
 bee_summary = df[
     [
         "Site",
@@ -102,36 +109,32 @@ bee_summary = df[
         "diversity_class_species"
     ]
 ]
-
 # Check it looks ok
 bee_summary.head()
 
 # Now merging the bee_summary data frame and the site_data data frame
 # This way, for each site, we have various measures of bee diversity and various site descriptors (e.g. water and forest cover)
 # We will then be able to look at associations between bee diversity and site characteristics
-
 bees = site_data.merge(
     bee_summary,
     on="Site",
     how="left"
 )
-
 # Checking the merged table looks ok
 bees.head()
 
 # I will remove columns that we won't be using in the model
 # I will remove State and County as they are categorical variables which I don't wish to include in the model
-# i will remove Northing and Eassting as we already have longitude and latitude
+# i will remove Northing and Eassting as we already have Longitude and Latitude
 bees = bees.drop(columns=["State", "County", "Northing", "Easting"])
 
-# Looking at the correlations between all the variables
+### LOOKING AT RELATIONSHIPS BETWEEN X VARIABLES ###
 
+# Looking at the correlations between all the variables
 # Select only numeric columns
 numeric_cols = bees.select_dtypes(include="number")
-
 # Compute correlation matrix
 corr = numeric_cols.corr()
-
 # Plot a heatmap
 plt.figure(figsize=(10, 8))
 sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm", square=True)
@@ -139,23 +142,24 @@ plt.title("Pairwise Correlation Between Numeric Variables")
 plt.show()
 
 # Multi-variable scatter matrix to further look at relationships between variables
+scatter_matrix = scatter_matrix(bees, figsize=(16, 16))
 
-from pandas.plotting import scatter_matrix
+### CHOOSING WHICH COLUMNS TO USE IN THE DATA FRAME WHICH THE MODEL WILL BE TRAINED ON ###
 
-a = scatter_matrix(bees, figsize=(16, 16))
-
+# Bee community columns:
 # We have several measures of bee diversity
 # For the purposes of this model, I will use total number of bee species as the metric (rather than simpsons diversity index)
 # The model will try to predict if a site has high or low diversity
 # High diversity = more species than the median across sites
 # Low diversoty = fewer species than the median across sites
 
-# The site data looks at the ground cover of habitats like wetland and forest in the area surrounding a transect (within 200m, 1000m)
+# Site feature columns:
+# The site data looks at proportion the ground cover of habitats like wetland and forest in the area surrounding a transect (within 200m, 1000m)
+# (the remaining proportion of the site not described as these different habitat types is agricultural)
 # Habitat cover at 200m and 1000m is strongly correlated for each site (makes sense)
-# We will exclude 200m cover and just use 1000m cover
+# We will exclude 200m cover columns and just use 1000m cover columns
 
 # Creating new data table with selected columns for model training:
-
 # Create a list of columns for modeling
 model_cols = [
     "Latitude",
@@ -167,41 +171,39 @@ model_cols = [
     "1000m_wetlands",
     "diversity_class_species"  # This is the target variable
 ]
-
 # Create new DataFrame for modeling
 model_data = bees[model_cols].copy()
-
 # Checking new data frame looks ok
 model_data.head()
 
-# Looking at colinearity again in this reduced dataframe
+### MAKING SURE MODEL DATA FRAME IS SUITABLE FOR MODELLING ###
 
+# Looking at colinearity again in this reduced dataframe:
 # Select only numeric columns
 numeric_cols2 = model_data.select_dtypes(include="number")
-
 # Compute correlation matrix
 corr2 = numeric_cols2.corr()
-
 # Plot a heatmap
 plt.figure(figsize=(6, 4))
 sns.heatmap(corr2, annot=True, fmt=".2f", cmap="coolwarm", square=True)
 plt.title("Pairwise Correlations")
 plt.show()
-
 # For now I will not be removing any variables based on possible colinearity, as I'd like to run a model including everything first
 
 # Converting the diversity class (high vs low) into a numeric variable
 # Convert high to 1, low to 0
 model_data["diversity_class_species"] = model_data["diversity_class_species"].map({"low": 0, "high": 1})
-
 # Checking this looks ok
 model_data.head()
 
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 # 3. Logistic regression model
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 
-# Preparing the data and training the logistic regression model
+### PREPARING THE DATA AND TRAINING THE LOGISTIC REGRESSION MODEL ###
+
+# Importing the model from sklearn
+from sklearn.linear_model import LogisticRegression
 
 # Splitting data into features (X) and the data we are trying to predict (y)
 X = model_data[['Latitude', 'Longitude', '1000m_open', '1000m_developed', '1000m_forest', '1000m_earlysuccessional', '1000m_wetlands']]
@@ -223,7 +225,7 @@ y_pred = model.predict(X_test)
 print("Prediction results of the first 5 test samples：", y_pred[:5])
 print("Top 5 real results：", y_test.values[:5])
 
-# Checking how well the model did
+### ASSESSING MODEL PERFORMANCE ###
 
 # Calculate model accuracy
 accuracy = accuracy_score(y_test, y_pred)
@@ -253,9 +255,10 @@ print(feature_importance)
 # Our model currently has an accuracy of 0.60 (not great)
 # I will try some tweaks to see if I can improve the model at all
 
+### CHECKING FEATURE IMPORTANCE AND EFFECT OF REMOVING FEATURES ON MODEL PERFORMANCE ###
+
 # We will try recursive feature elimination (removing features which do not improve the model's accuracy)
-# We import recursive feature removal from sklearn for this
-from sklearn.feature_selection import RFE
+# We have imported recursive feature removal from sklearn for this
 
 # Rank all features by their importance
 rfe = RFE(estimator=model, n_features_to_select=1)
@@ -266,7 +269,7 @@ feature_names = ['Latitude', 'Longitude', '1000m_open', '1000m_developed', '1000
                  '1000m_earlysuccessional', '1000m_wetlands']
 
 # Rank features using RFE (1 = most important)
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression()
 rfe = RFE(estimator=model, n_features_to_select=1)
 rfe.fit(X_train, y_train)
 
@@ -294,7 +297,7 @@ for i in range(len(features_sorted_by_rank)):
     X_test_selected = X_test[:, indices]
 
     # Train logistic regression
-    model_iter = LogisticRegression(max_iter=1000)
+    model_iter = LogisticRegression()
     model_iter.fit(X_train_selected, y_train)
 
     # Compute test accuracy
@@ -313,9 +316,11 @@ print(results_df)
 
 # It appears that removing features will not improve the model's accuracy
 
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 # 4. Random forest model
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
+
+### TRAINING THE RANDOM FOREST MODEL ###
 
 # Importing the model from sklearn
 from sklearn.ensemble import RandomForestClassifier
@@ -328,6 +333,8 @@ rf_model.fit(X_train, y_train)
 y_pred = rf_model.predict(X_test)
 print("Prediction results of the first 5 test samples:", y_pred[:5])
 print("Top 5 real results:", y_test.values[:5])
+
+### ASSESSING MODEL PERFORMANCE ###
 
 # Evaluate model
 accuracy = accuracy_score(y_test, y_pred)
@@ -349,6 +356,8 @@ plt.show()
 # Bottom left: false negatives (site has high diversity, but model predicts low): 4
 # Bottom right: true positives: 12
 
+### CHECKING FEATURE IMPORTANCE ###
+
 # Feature importance
 feature_importance = pd.DataFrame({
     'Feature': X.columns,
@@ -357,9 +366,11 @@ feature_importance = pd.DataFrame({
 print("Random Forest feature importance:")
 print(feature_importance)
 
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
 # 5. K means clustering model
-# -----------------------------
+# ------------------------------------------------------------------------------------- #
+
+### TRAINING THE K MEANS CLUSTERING MODEL ###
 
 # Importing model from sklearn
 from sklearn.cluster import KMeans
@@ -379,11 +390,16 @@ print("Cluster assignments for first 10 sites:", clusters[:10])
 clustered_data = model_data.copy()
 clustered_data['Cluster'] = clusters
 
+### ASSESSING MODEL PERFORMANCE ###
+
 # Compare clusters to actual diversity class
 
 # Confusion matrix between clusters and actual classes
 cm = confusion_matrix(clustered_data['diversity_class_species'], clustered_data['Cluster'])
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[0,1])
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm,
+    display_labels=["Low diversity", "High diversity"]
+)
 disp.plot(cmap='Blues')
 plt.title('K-Means Clusters vs Actual Diversity Class')
 plt.show()
@@ -396,8 +412,7 @@ plt.show()
 # Bottom left: mismatch (we label 1, cluster labels 0): 34
 # Bottom right: both label and cluster = 1: 14
 
-# Importing adjusted rand score from sklearn - this lets us check how similar the clustering is to our labels
-from sklearn.metrics import adjusted_rand_score
+# Imported adjusted rand score from sklearn - this lets us check how similar the clustering is to our labels
 
 # Adjusted Rand Index (measures similarity between clusters and true labels, 1 = perfect match)
 ari = adjusted_rand_score(clustered_data['diversity_class_species'], clustered_data['Cluster'])
